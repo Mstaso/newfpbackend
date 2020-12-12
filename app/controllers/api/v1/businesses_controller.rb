76ef007f80242821c57accfe92543e9f6fc7ff7d
malcolm <mstaso1@gmail.com>
2020-12-12 12:28:09 -0500

    business model switched to include users from matches

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::BusinessesController < ApplicationController
     
     def index
         @businesses = Business.all
-        render json: @businesses, include: [:courses,:matches], status: :ok
+        render json: @businesses, include: [:courses, :users], status: :ok
     end
 end
